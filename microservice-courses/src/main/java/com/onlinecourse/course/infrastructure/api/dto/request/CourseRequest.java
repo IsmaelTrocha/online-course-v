372541794a0db23190741fd0b33f2e942c02e8e2
Ismael Trocha <ismael.trocha@hotmail.com>
2024-03-15 00:20:25 -0500

    Course Creation End point is working successfully.

--- microservice-courses/src/main/java/com/onlinecourse/course/infrastructure/api/dto/request/CourseRequest.java
+++ microservice-courses/src/main/java/com/onlinecourse/course/infrastructure/api/dto/request/CourseRequest.java
@@ -1,10 +1,8 @@
 package com.onlinecourse.course.infrastructure.api.dto.request;
 
-import com.onlinecourse.course.domain.entities.CourseContent;
 import com.onlinecourse.course.domain.entities.Language;
 import com.onlinecourse.course.domain.entities.Level;
 import java.time.LocalDateTime;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +12,6 @@ import lombok.NoArgsConstructor;
 @Data
 public class CourseRequest {
 
-  private Long id;
   private String title;
   private String description;
   private Language language;
